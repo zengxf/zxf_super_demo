2d51a29cc07b0080732fba038059ce737eee78a2
zengxf <fl_zxf@163.com>
2017-05-27 10:19:56 +0800

    akka , jsqlparser , tensorflow demo

--- simple-demo/src/cn/simple/test/temp/TempTest.java
+++ simple-demo/src/cn/simple/test/temp/TempTest.java
@@ -2,18 +2,14 @@
 package cn.simple.test.temp;
 
 import java.io.IOException;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 public class TempTest {
 
     public static void main( String[] args ) throws IOException {
-	Stream.of(1,2)
-	.map( Integer::toBinaryString )
-	.collect( Collectors.toList() );
+	System.out.println( 1 );
+	System.out.println( ~1 );
+	System.out.println( 1 ^ 0xFFFFFFFF );
+	System.out.println( 0xFFFFFFFF );
     }
 
 }
