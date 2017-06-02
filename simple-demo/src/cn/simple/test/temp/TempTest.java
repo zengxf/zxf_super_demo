24b8bb960f2436d5a82eaec695cfb1521c9fcab6
zengxf <fl_zxf@163.com>
2017-06-02 16:56:37 +0800

    simple up

--- simple-demo/src/cn/simple/test/temp/TempTest.java
+++ simple-demo/src/cn/simple/test/temp/TempTest.java
@@ -6,10 +6,6 @@ import java.io.IOException;
 public class TempTest {
 
     public static void main( String[] args ) throws IOException {
-	System.out.println( 1 );
-	System.out.println( ~1 );
-	System.out.println( 1 ^ 0xFFFFFFFF );
-	System.out.println( 0xFFFFFFFF );
     }
 
 }
