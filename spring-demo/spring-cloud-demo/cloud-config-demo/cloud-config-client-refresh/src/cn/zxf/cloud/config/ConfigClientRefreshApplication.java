18c43e7d9dcc4ba88157e80600d69ac9e8b0cbf3
zengxf <fl_zxf@163.com>
2017-06-29 10:42:30 +0800

    cloud config

---
+++ spring-demo/spring-cloud-demo/cloud-config-demo/cloud-config-client-refresh/src/cn/zxf/cloud/config/ConfigClientRefreshApplication.java
@@ -0,0 +1,13 @@
+package cn.zxf.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientRefreshApplication {
+
+    public static void main( String[] args ) {
+	SpringApplication.run( ConfigClientRefreshApplication.class, args );
+    }
+    
+}
