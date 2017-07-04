87da2f2e33dc1e2066a7eaa70276348abaebdd4e
zengxf <fl_zxf@163.com>
2017-07-04 10:51:45 +0800

    java8 Optional & CompletableFuture

--- simple-demo/src/cn/simple/test/new_features/jdk18/TestOptional.java
+++ simple-demo/src/cn/simple/test/new_features/jdk18/TestOptional.java
@@ -8,15 +8,28 @@ import java.util.Optional;
  * @author zengxf
  */
 public class TestOptional {
-    public static void main( String[] args ) {
+    public static void main( String[] args ) throws Throwable {
 	Optional<String> optional = Optional.of( "BAM" );
 	System.out.println( optional.isPresent() ); // true
 	System.out.println( optional.get() ); // "BAM"
 	System.out.println( optional.orElse( "fallback" ) ); // "BAM"
 	optional.ifPresent( ( s ) -> System.out.println( s.charAt( 0 ) ) ); // "B"
+
+	System.out.println( "-------------" );
+	map();
+	System.out.println( "-------------" );
+	flatMap();
     }
 
-    static void maina() throws Throwable {
+    static void map() throws Throwable {
+	Optional.of( new Outer() )//
+	        .map( o -> o.nested )//
+	        .map( n -> n.inner )//
+	        .map( i -> i.foo )//
+	        .ifPresent( System.out::println );
+    }
+
+    static void flatMap() throws Throwable {
 	Optional.of( new Outer() )//
 	        .flatMap( o -> Optional.ofNullable( o.nested ) )//
 	        .flatMap( n -> Optional.ofNullable( n.inner ) )//
