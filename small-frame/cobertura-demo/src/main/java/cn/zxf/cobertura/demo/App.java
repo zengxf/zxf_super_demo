67ef47741bdda1f158ca4f1c73ec05a62b5d75eb
zengxf <fl_zxf@163.com>
2017-07-13 15:34:23 +0800

    测试覆盖率 cobertura demo

---
+++ small-frame/cobertura-demo/src/main/java/cn/zxf/cobertura/demo/App.java
@@ -0,0 +1,13 @@
+package cn.zxf.cobertura.demo;
+
+public class App {
+    public int add( int a, int b ) {
+	return a + b;
+    }
+
+    public int subtract( int a, int b ) {
+	int c = 10;
+	a *= c;
+	return a - b;
+    }
+}
---
+++ small-frame/cobertura-demo/src/test/java/cn/zxf/cobertura/demo/AppTest.java
@@ -0,0 +1,37 @@
+package cn.zxf.cobertura.demo;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AppTest {
+    private App app;
+
+    @Before
+    public void setUp() {
+	app = new App();
+    }
+
+    @Test
+    public void testAdd() throws InterruptedException {
+	int a = 1;
+	int b = 2;
+	int result = app.add( a, b );
+	Assert.assertEquals( a + b, result );
+    }
+
+    @Test( )
+    public void testSubtract() throws InterruptedException {
+	int a = 1;
+	int b = 2;
+	int result = app.subtract( a, b );
+	System.out.println( "\nresult: " + result );
+	System.out.println();
+	// Assert.assertEquals( a - b, result );
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
