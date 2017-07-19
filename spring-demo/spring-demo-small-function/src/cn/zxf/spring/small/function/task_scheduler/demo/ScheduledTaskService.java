9e5fae94f91ec596edc6c0c2b576dd39b1ac65ef
zengxf <fl_zxf@163.com>
2017-07-19 16:41:56 +0800

    schedule doc

--- spring-demo/spring-demo-small-function/src/cn/zxf/spring/small/function/task_scheduler/demo/ScheduledTaskService.java
+++ spring-demo/spring-demo-small-function/src/cn/zxf/spring/small/function/task_scheduler/demo/ScheduledTaskService.java
@@ -11,6 +11,11 @@ public class ScheduledTaskService {
 
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat( "HH:mm:ss" );
 
+    /**
+     * @Scheduled(fixedRate = 6000) ：上一次开始执行时间点之后6秒再执行
+	 * @Scheduled(fixedDelay = 6000) ：上一次执行完毕时间点之后6秒再执行
+	 * @Scheduled(initialDelay=1000, fixedRate=6000) ：第一次延迟1秒后执行，之后按fixedRate的规则每6秒执行一次
+	*/
     @Scheduled( fixedRate = 5000 ) // 1
     public void reportCurrentTime() {
 	System.out.println( "每隔五秒执行一次 " + dateFormat.format( new Date() ) );
