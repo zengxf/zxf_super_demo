aeb173e0472d50968bff1e8dc7a8144c5b1d0cbd
zengxf <fl_zxf@163.com>
2017-08-01 16:53:29 +0800

    断言

--- small-frame/junit-demo/junit4-demo/test/test/cn/zxf/junit/demo1/TestAssert.java
+++ small-frame/junit-demo/junit4-demo/test/test/cn/zxf/junit/demo1/TestAssert.java
@@ -5,13 +5,15 @@ import static org.junit.Assert.assertThat;
 
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 public class TestAssert {
 
     @Test
     public void test1() {
 	assertThat( true, is( true ) );
-	assertThat( "a", is( "a" ) );
+	String value = "a";
+	assertThat( "应该为 a", value, is( "a" ) );
     }
 
     @Test
@@ -19,4 +21,13 @@ public class TestAssert {
     public void somethingWeCannotHandleRightNow() {
 	System.out.println( "--------" );
     }
+
+    @Test
+    @Category( TestAssert.class ) // 此分类没什么用
+    public void test2() {
+	System.out.println( "--------" );
+	String value = "a";
+	assertThat( "应该为 a", value, is( "a" ) );
+    }
+
 }
