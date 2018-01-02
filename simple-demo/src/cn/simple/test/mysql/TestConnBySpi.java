95925e6546ad0f620c245d27eea393c4d7b93b4c
zengxf <fl_zxf@163.com>
2018-01-02 18:11:17 +0800

    jdbc-spi

--- simple-demo/src/cn/simple/test/mysql/TestConnBySpi.java
+++ simple-demo/src/cn/simple/test/mysql/TestConnBySpi.java
@@ -4,22 +4,26 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import com.mysql.jdbc.Driver;
+/**
+ * 测试 - 通过 SPI 获取连接 <br>
+ * 不再需要调用 Class.forName("com.mysql.jdbc.Driver")
+ * 
+ * <p>
+ * Created by zengxf on 2018-01-02
+ */
+public class TestConnBySpi {
 
-public class ConnectionUtil {
+    public static void main( String[] args ) {
+        Connection conn = getLocalConnection();
+        System.out.println( conn );
+    }
 
-    /**
-     * 获取本地连接
-     * 
-     * @return
-     */
-    public static Connection getLocalConnection() {
+    static Connection getLocalConnection() {
         try {
             String url = "jdbc:mysql://localhost:3306/zxf_dev";
             String user = "root";
             String password = "admin";
 
-            Driver.class.getName();
             Connection conn = DriverManager.getConnection( url, user, password );
 
             return conn;
