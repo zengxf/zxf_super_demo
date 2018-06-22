4378e4c4443a8995a9bd91c8f5cd61480e348e1b
zengxf <fl_zxf@163.com>
2018-06-22 18:26:40 +0800

    mysql demo

---
+++ small-frame/jdbc-demo/mysql-jdbc-demo/src/main/java/cn/zxf/mysql/TestConnBySpi.java
@@ -0,0 +1,36 @@
+package cn.zxf.mysql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * 测试 - 通过 SPI 获取连接 <br>
+ * 不再需要调用 Class.forName("com.mysql.jdbc.Driver")
+ * 
+ * <p>
+ * Created by zengxf on 2018-01-02
+ */
+public class TestConnBySpi {
+
+    public static void main( String[] args ) {
+        Connection conn = getLocalConnection();
+        System.out.println( conn );
+    }
+
+    static Connection getLocalConnection() {
+        try {
+            String url = "jdbc:mysql://localhost:3306/zxf_dev?useUnicode=true&characterEncoding=utf8&serverTimezone=UTC";
+            String user = "root";
+            String password = "admin";
+
+            Connection conn = DriverManager.getConnection( url, user, password );
+
+            return conn;
+        } catch ( SQLException e ) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
