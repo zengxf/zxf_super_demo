4b506dfae1f051da90e0022ae54ee677de7f4e0b
zengxf <fl_zxf@163.com>
2019-04-12 19:08:27 +0800

    resolve dead lock

--- simple-demo/src/main/java/cn/simple/test/string/encrypt/Base64DecodeStr.java
+++ simple-demo/src/main/java/cn/simple/test/string/encrypt/Base64DecodeStr.java
@@ -5,8 +5,9 @@ import java.util.Base64;
 
 public class Base64DecodeStr {
     public static void main( String[] args ) throws IOException {
-	String str = "1U5529EdanIXcx71UUUUU7vcSsGvfC2KfnxnUU==";
-	byte[] arr = Base64.getDecoder().decode( str );
-	System.out.println( new String( arr, "GB2312" ) );
+        String str = "1U5529EdanIXcx71UUUUU7vcSsGvfC2KfnxnUU==";
+        byte[] arr = Base64.getDecoder()
+                .decode( str );
+        System.out.println( new String( arr, "UTF-8" ) );
     }
 }
--- simple-demo/src/main/java/cn/simple/test/string/encrypt/pfx/ReadPFX.java
+++ simple-demo/src/main/java/cn/simple/test/string/encrypt/pfx/ReadPFX.java
@@ -15,88 +15,83 @@ public class ReadPFX {
 
     // 转换成十六进制字符串
     public String Byte2String( byte[] b ) {
-	String hs = "";
-	String stmp = "";
+        String hs = "";
+        String stmp = "";
 
-	for ( int n = 0; n < b.length; n++ ) {
-	    stmp = ( java.lang.Integer.toHexString( b[n] & 0XFF ) );
-	    if ( stmp.length() == 1 )
-		hs = hs + "0" + stmp;
-	    else
-		hs = hs + stmp;
-	    // if (n<b.length-1) hs=hs+":";
-	}
-	return hs.toUpperCase();
+        for ( int n = 0; n < b.length; n++ ) {
+            stmp = ( java.lang.Integer.toHexString( b[n] & 0XFF ) );
+            if ( stmp.length() == 1 )
+                hs = hs + "0" + stmp;
+            else
+                hs = hs + stmp;
+        }
+        return hs.toUpperCase();
     }
 
     public byte[] StringToByte( int number ) {
-	int temp = number;
-	byte[] b = new byte[4];
-	for ( int i = b.length - 1; i > -1; i-- ) {
-	    b[i] = new Integer( temp & 0xff ).byteValue();// 将最高位保存在最低位
-	    temp = temp >> 8; // 向右移8位
-	}
-	return b;
+        int temp = number;
+        byte[] b = new byte[4];
+        for ( int i = b.length - 1; i > -1; i-- ) {
+            b[i] = Integer.valueOf( temp & 0xff )
+                    .byteValue();// 将最高位保存在最低位
+            temp = temp >> 8; // 向右移8位
+        }
+        return b;
     }
 
     public PrivateKey GetPvkformPfx( String strPfx, String strPassword ) {
-	try {
-	    KeyStore ks = KeyStore.getInstance( "PKCS12" );
-	    FileInputStream fis = new FileInputStream( strPfx );
-	    // If the keystore password is empty(""), then we have to set
-	    // to null, otherwise it won't work!!!
-	    char[] nPassword = null;
-	    if ( ( strPassword == null ) || strPassword.trim().equals( "" ) ) {
-		nPassword = null;
-	    } else {
-		nPassword = strPassword.toCharArray();
-	    }
-	    ks.load( fis, nPassword );
-	    fis.close();
-	    System.out.println( "keystore type=" + ks.getType() );
-	    // Now we loop all the aliases, we need the alias to get keys.
-	    // It seems that this value is the "Friendly name" field in the
-	    // detals tab <-- Certificate window <-- view <-- Certificate
-	    // Button <-- Content tab <-- Internet Options <-- Tools menu
-	    // In MS IE 6.
-	    Enumeration<String> enumas = ks.aliases();
-	    String keyAlias = null;
-	    if ( enumas.hasMoreElements() )// we are readin just one certificate.
-	    {
-		keyAlias = (String) enumas.nextElement();
-		System.out.println( "alias=[" + keyAlias + "]" );
-	    }
-	    // Now once we know the alias, we could get the keys.
-	    System.out.println( "is key entry=" + ks.isKeyEntry( keyAlias ) );
-	    PrivateKey prikey = (PrivateKey) ks.getKey( keyAlias, nPassword );
-	    Certificate cert = ks.getCertificate( keyAlias );
-	    PublicKey pubkey = cert.getPublicKey();
+        try {
+            KeyStore ks = KeyStore.getInstance( "PKCS12" );
+            FileInputStream fis = new FileInputStream( strPfx );
+            char[] nPassword = null;
+            if ( ( strPassword == null ) || strPassword.trim()
+                    .equals( "" ) ) {
+                nPassword = null;
+            } else {
+                nPassword = strPassword.toCharArray();
+            }
+            ks.load( fis, nPassword );
+            fis.close();
+            System.out.println( "keystore type=" + ks.getType() );
+            Enumeration<String> enumas = ks.aliases();
+            String keyAlias = null;
+            if ( enumas.hasMoreElements() ) {
+                keyAlias = (String) enumas.nextElement();
+                System.out.println( "alias=[" + keyAlias + "]" );
+            }
+            // Now once we know the alias, we could get the keys.
+            System.out.println( "is key entry=" + ks.isKeyEntry( keyAlias ) );
+            PrivateKey prikey = (PrivateKey) ks.getKey( keyAlias, nPassword );
+            Certificate cert = ks.getCertificate( keyAlias );
+            PublicKey pubkey = cert.getPublicKey();
 
-	    System.out.println( "cert class = " + cert.getClass().getName() );
-	    System.out.println( "cert = " + cert );
-	    System.out.println( "public key = " + pubkey );
-	    System.out.println( "private key = " + prikey );
+            System.out.println( "cert class = " + cert.getClass()
+                    .getName() );
+            System.out.println( "cert = " + cert );
+            System.out.println( "public key = " + pubkey );
+            System.out.println( "private key = " + prikey );
 
-	    System.out.println( "pub" );
-	    this.outKey( pubkey );
-	    System.out.println( "pri" );
-	    this.outKey( prikey );
+            System.out.println( "pub" );
+            this.outKey( pubkey );
+            System.out.println( "pri" );
+            this.outKey( prikey );
 
-	    return prikey;
-	} catch ( Exception e ) {
-	    e.printStackTrace();
-	}
-	return null;
+            return prikey;
+        } catch ( Exception e ) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     private void outKey( Key key ) {
-	byte[] keys = key.getEncoded();
-	byte[] enc = Base64.getEncoder().encode( keys );
-	for ( byte b : enc ) {
-	    String s = String.format( "0x%2x,", b );
-	    System.out.print( s );
-	}
-	System.out.println();
+        byte[] keys = key.getEncoded();
+        byte[] enc = Base64.getEncoder()
+                .encode( keys );
+        for ( byte b : enc ) {
+            String s = String.format( "0x%2x,", b );
+            System.out.print( s );
+        }
+        System.out.println();
     }
 
 }
