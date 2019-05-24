30ce4a391aebb6e3c737a3a14ab7f461f0f2370f
zengxf <fl_zxf@163.com>
2019-05-24 15:04:41 +0800

    bridge method test

--- simple-demo/src/main/java/cn/simple/test/temp/TempTest.java
+++ simple-demo/src/main/java/cn/simple/test/temp/TempTest.java
@@ -1,5 +1,7 @@
 package cn.simple.test.temp;
 
+import java.util.stream.Stream;
+
 import lombok.extern.slf4j.Slf4j;
 
 // M:\project\zxf_super_demo\simple-demo\bin\cn\simple\test\temp
@@ -7,10 +9,10 @@ import lombok.extern.slf4j.Slf4j;
 public class TempTest {
 
     public static void main( String[] args ) throws InterruptedException {
-        long l1 = System.currentTimeMillis() << 32;
-        System.out.println( Long.toString( l1, 2 ) );
-        System.out.println( Long.toString( l1, 36 ) );
-        log.info("ddd");
+        Stream.of( 23, 4, 54 )
+                .forEach( i -> {
+                    log.info( "{}", i );
+                } );
     }
 
 }
