5990d94d3027d7709fb54154b70898983c73fb6c
zengxf <fl_zxf@163.com>
2019-05-30 09:56:26 +0800

    format code

--- simple-demo/src/main/java/cn/simple/test/jdkapi/io/file/ImageToBase64.java
+++ simple-demo/src/main/java/cn/simple/test/jdkapi/io/file/ImageToBase64.java
@@ -10,11 +10,13 @@ import java.util.Base64;
 
 public class ImageToBase64 {
     public static void main( String[] args ) throws URISyntaxException, IOException {
-	Path src = Paths.get( ImageToBase64.class.getResource( "hunterplus.png" ).toURI() );
-	ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
-	Files.copy( src, byteOut );
-	byte[] content = byteOut.toByteArray();
-	String b64 = Base64.getEncoder().encodeToString( content );
-	System.out.println( b64 );
+        Path src = Paths.get( ImageToBase64.class.getResource( "hunterplus.png" )
+                .toURI() );
+        ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
+        Files.copy( src, byteOut );
+        byte[] content = byteOut.toByteArray();
+        String b64 = Base64.getEncoder()
+                .encodeToString( content );
+        System.out.println( b64 );
     }
 }
--- simple-demo/src/main/java/cn/simple/test/jdkapi/io/file/jar/ReadJarFile.java
+++ simple-demo/src/main/java/cn/simple/test/jdkapi/io/file/jar/ReadJarFile.java
@@ -17,48 +17,53 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ReadJarFile {
     public static void main( String[] args ) throws MalformedURLException {
-	readByIO();
-	System.out.println( "=======================" );
-	readByUri();
-	System.out.println( "=======================" );
-	readByUrl();
+        readByIO();
+        System.out.println( "=======================" );
+        readByUri();
+        System.out.println( "=======================" );
+        readByUrl();
     }
 
     static void readByIO() {
-	try {
-	    URI uri = ReadJarFile.class.getResource( "jar-content.txt" ).toURI();
-	    log.info( "readByIO uri 1: [{}]", uri );
-	    String template = IOUtils.toString( uri, "GBK" );
-	    System.out.println( template );
-	} catch ( Exception e ) {
-	    e.printStackTrace();
-	}
+        try {
+            URI uri = ReadJarFile.class.getResource( "jar-content.txt" )
+                    .toURI();
+            log.info( "readByIO uri 1: [{}]", uri );
+            String template = IOUtils.toString( uri, "GBK" );
+            System.out.println( template );
+        } catch ( Exception e ) {
+            e.printStackTrace();
+        }
     }
 
     @Deprecated // jar 包内读取有问题
     static void readByUri() {
-	try {
-	    URI uri = ReadJarFile.class.getResource( "jar-content.txt" ).toURI();
-	    log.info( "readByUri uri 1: [{}]", uri );
-	    Path path = Paths.get( uri );
-	    String template = Files.readAllLines( path, Charset.forName( "GBK" ) ).stream().collect( Collectors.joining( "\n" ) );
-	    System.out.println( template );
-	} catch ( Exception e ) {
-	    e.printStackTrace();
-	}
+        try {
+            URI uri = ReadJarFile.class.getResource( "jar-content.txt" )
+                    .toURI();
+            log.info( "readByUri uri 1: [{}]", uri );
+            Path path = Paths.get( uri );
+            String template = Files.readAllLines( path, Charset.forName( "GBK" ) )
+                    .stream()
+                    .collect( Collectors.joining( "\n" ) );
+            System.out.println( template );
+        } catch ( Exception e ) {
+            e.printStackTrace();
+        }
     }
 
     static void readByUrl() {
-	try {
-	    URI path = ReadJarFile.class.getResource( "jar-content.txt" ).toURI();
-	    log.info( "readByUrl uri 1: [{}]", path );
-	    URL url = path.toURL();
-	    InputStream is = url.openStream();
-	    byte[] bys = new byte[1024_00];
-	    int length = is.read( bys );
-	    System.out.println( new String( bys, 0, length ) );
-	} catch ( Exception e ) {
-	    e.printStackTrace();
-	}
+        try {
+            URI path = ReadJarFile.class.getResource( "jar-content.txt" )
+                    .toURI();
+            log.info( "readByUrl uri 1: [{}]", path );
+            URL url = path.toURL();
+            InputStream is = url.openStream();
+            byte[] bys = new byte[1024_00];
+            int length = is.read( bys );
+            System.out.println( new String( bys, 0, length ) );
+        } catch ( Exception e ) {
+            e.printStackTrace();
+        }
     }
 }
