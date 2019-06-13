99db454b3e597c2cc1ac6960a10b869b4c497fbb
zengxf <fl_zxf@163.com>
2019-06-13 12:16:36 +0800

    remote debug & monitor

--- simple-demo/src/main/java/cn/simple/test/jvm/remote/debug/TestRemoteDebug.java
+++ simple-demo/src/main/java/cn/simple/test/jvm/remote/debug/TestRemoteDebug.java
@@ -7,16 +7,18 @@ package cn.simple.test.jvm.remote.debug;
  * JVM 参数：
  * 
  * <pre>
- *  -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8000      // >= jdk1.5
- *  -Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=8000             // jdk1.4
- *  -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8002   // 等到eclipse连接，服务才能用
+ *  `-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8000`      // >= jdk1.5
+ *  `-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000`   // jdk1.4
+ *      `suspend=y` 等到 eclipse 连接，服务才能用，默认为 `y`
+ *      `address=0.0.0.0:8000` 默认 IP 是 127.0.0.1，可用  `netstat -na | grep 8000` 查看
  *  测试：
- *      java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8000 cn.simple.test.jvm.remote.debug.TestRemoteDebug
+ *      `java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8000 cn.simple.test.jvm.remote.debug.TestRemoteDebug`
  * </pre>
  * 
  * <p>
  * Created by zengxf on 2018-03-19
  */
+// M:\project\zxf_super_demo\simple-demo\bin\main\cn\simple\test\jvm\remote\debug
 public class TestRemoteDebug {
 
     public static void main( String[] args ) throws InterruptedException {
--- simple-demo/src/main/java/cn/simple/test/jvm/remote/monitor/TestJvmRemoteMonitor.java
+++ simple-demo/src/main/java/cn/simple/test/jvm/remote/monitor/TestJvmRemoteMonitor.java
@@ -2,57 +2,83 @@ package cn.simple.test.jvm.remote.monitor;
 
 import java.util.Map;
 import java.util.WeakHashMap;
-
-import cn.simple.util.SleepUtils;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 远程 JVM 监控
  * <p>
  * 
- * JVM 参数
- * 
  * <pre>
-    -Djava.rmi.server.hostname=127.0.0.1
-    -Djava.rmi.server.hostname=192.168.1.161
-    -Dcom.sun.management.jmxremote
-    -Dcom.sun.management.jmxremote.port=8888
-    -Dcom.sun.management.jmxremote.authenticate=false
-    -Dcom.sun.management.jmxremote.ssl=false
-    
-    -Djava.rmi.server.hostname=xx.xx.xx.xx
-    -Dcom.sun.management.jmxremote 
-    -Dcom.sun.management.jmxremote.authenticate=false 
-    -Dcom.sun.management.jmxremote.ssl=false
-    -Dcom.sun.management.jmxremote.port=7969
+   ----
+   No Authenticate 无密码无认证：
+   Start 命令：                                                    
+       java                                                  \
+          -Dcom.sun.management.jmxremote                     \
+          -Djava.rmi.server.hostname=192.168.1.117           \
+          -Dcom.sun.management.jmxremote.port=8888           \
+          -Dcom.sun.management.jmxremote.ssl=false           \
+          -Dcom.sun.management.jmxremote.authenticate=false  \
+          cn.simple.test.jvm.remote.monitor.TestJvmRemoteMonitor
+   ----
+   JConsole 连接：
+       192.168.1.117:8888
+   VisualVM 连接：
+       Add Remote Host：192.168.1.117
+       Add JMX Connection：8888
  * </pre>
  * 
- * 设置密码
- * 
  * <pre>
- * echo "admin 123" > /base-data/jdk1.8.0_91/jre/lib/management/jmxremote.password    
- * 修改文件权限                                                                             
- * chmod 0400 /base-data/jdk1.8.0_91/jre/lib/management/jmxremote.password
+   ----
+   Authenticate 密码认证：
+       Reference: http://www.voidcn.com/article/p-qnwtydxi-bkw.html
+   Authenticate 文件配置：
+       printf "admin test \nmonitor test \n" > password.txt
+       printf "admin readwrite \n monitor readonly \n" > access.txt
+       chmod 0400 password.txt
+       Explain 解释：`readwrite` 可操作，如执行 GC；`readonly` 只能查看。密码文件用户名前不能有空格
+   Start 命令：                                                    
+       java                                                  \
+          -Dcom.sun.management.jmxremote                     \
+          -Djava.rmi.server.hostname=192.168.1.117           \
+          -Dcom.sun.management.jmxremote.port=8888           \
+          -Dcom.sun.management.jmxremote.ssl=false           \
+          -Dcom.sun.management.jmxremote.authenticate=true   \
+          -Dcom.sun.management.jmxremote.password.file=password.txt \
+          -Dcom.sun.management.jmxremote.access.file=access.txt     \
+          cn.simple.test.jvm.remote.monitor.TestJvmRemoteMonitor
+   ----
+   JConsole 连接：
+       192.168.1.117:8888            # 设置用户名和密码
+   VisualVM 连接：
+       Add Remote Host：192.168.1.117
+       Add JMX Connection：8888      # 设置用户名和密码
  * </pre>
  * 
- * JConsole 连接
- * 
  * <pre>
- * 192.168.1.161:8888
+ * 其他问题：
+ *     不要启用 ssl
  * </pre>
- * 
  * <p>
  * Created by zengxf on 2018-03-19
  */
+// M:\project\zxf_super_demo\simple-demo\bin\main\cn\simple\test\jvm\remote\monitor
 public class TestJvmRemoteMonitor {
 
     public static class HoldMemoryTask implements Runnable {
-        static Map<Long, byte[]> map = new WeakHashMap<Long, byte[]>();
+        static Map<Long, byte[]> map  = new WeakHashMap<Long, byte[]>();
+
+        int                      sign = 0;
 
         @Override
         public void run() {
             while ( true ) {
                 map.put( System.nanoTime(), new byte[512] );
-                SleepUtils.millisecond( 1 );
+                millisecond( 1 );
+                if ( sign++ == 5000 ) {
+                    sign = 0;
+                    map.clear();
+                    System.out.printf( "[%tT] map-clear! %n", System.currentTimeMillis() );
+                }
             }
         }
     }
@@ -60,6 +86,14 @@ public class TestJvmRemoteMonitor {
     public static void main( String[] args ) {
         new Thread( new HoldMemoryTask(), "test-001" ).start();
         System.out.println( "test start" );
+    }
+
+    public static final void millisecond( long milliseconds ) {
+        try {
+            TimeUnit.MILLISECONDS.sleep( milliseconds );
+        } catch ( InterruptedException e ) {
+            e.printStackTrace();
+        }
     }
 
 }
