d3fdbbf81079ebc716e8b41f277b20732115f880
zengxf <fl_zxf@163.com>
2019-06-27 10:28:55 +0800

    当出现堆溢出 OOM 时 - 当前线程会挂 - 其他线程还能运行

--- simple-demo/src/main/java/cn/simple/test/temp/TempTest.java
+++ simple-demo/src/main/java/cn/simple/test/temp/TempTest.java
@@ -1,6 +1,6 @@
 package cn.simple.test.temp;
 
-// M:\project\zxf_super_demo\simple-demo\bin\cn\simple\test\temp
+// M:\project\zxf_super_demo\simple-demo\bin\main\cn\simple\test\temp
 public class TempTest {
 
     public static void main( String[] args ) {
