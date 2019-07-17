db56081d51d6e8f2d549715324cdc816a05c5c5d
zengxf <fl_zxf@163.com>
2019-07-17 18:10:22 +0800

    2038 年问题 测试

--- simple-demo/src/main/java/cn/simple/test/temp/TempTest.java
+++ simple-demo/src/main/java/cn/simple/test/temp/TempTest.java
@@ -1,18 +1,12 @@
 package cn.simple.test.temp;
 
-import java.util.ArrayList;
-import java.util.List;
-
 // M:\project\zxf_super_demo\simple-demo\bin\main\cn\simple\test\temp
 public class TempTest {
 
     public static void main( String[] args ) {
-        List<String> list = new ArrayList<>();
-        list.add( "dd" );
-        list.add( 0, "cc" );
-        list.add( 0, "bb" );
-        list.add( 0, "aa" );
-        System.out.println( list );
+        System.out.println( ( Long.MAX_VALUE - System.currentTimeMillis() ) / 1000L / 3600L / 24L / 366L );
+        System.out.printf( "%tF %<tT %n", ( 1000L * Integer.MAX_VALUE ) );
+        System.out.printf( "%tF %<tT %n", ( 1000L * Integer.MIN_VALUE ) );
     }
 
 }
