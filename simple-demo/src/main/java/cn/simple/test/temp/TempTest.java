f9d2291b83bb0e898c706ef19fcc350063f4d503
zengxf <fl_zxf@163.com>
2019-07-17 15:54:03 +0800

    test list add(0, v)

--- simple-demo/src/main/java/cn/simple/test/temp/TempTest.java
+++ simple-demo/src/main/java/cn/simple/test/temp/TempTest.java
@@ -1,9 +1,18 @@
 package cn.simple.test.temp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // M:\project\zxf_super_demo\simple-demo\bin\main\cn\simple\test\temp
 public class TempTest {
 
     public static void main( String[] args ) {
+        List<String> list = new ArrayList<>();
+        list.add( "dd" );
+        list.add( 0, "cc" );
+        list.add( 0, "bb" );
+        list.add( 0, "aa" );
+        System.out.println( list );
     }
 
 }
