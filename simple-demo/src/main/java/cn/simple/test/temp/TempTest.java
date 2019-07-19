d194b3bcaebe015d665f260fe6a6a0558c0f9921
zengxf <fl_zxf@163.com>
2019-07-19 12:28:12 +0800

    两值相加等于特定值

--- simple-demo/src/main/java/cn/simple/test/temp/TempTest.java
+++ simple-demo/src/main/java/cn/simple/test/temp/TempTest.java
@@ -1,12 +1,31 @@
 package cn.simple.test.temp;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.IntStream;
+
 // M:\project\zxf_super_demo\simple-demo\bin\main\cn\simple\test\temp
 public class TempTest {
 
     public static void main( String[] args ) {
-        System.out.println( ( Long.MAX_VALUE - System.currentTimeMillis() ) / 1000L / 3600L / 24L / 366L );
-        System.out.printf( "%tF %<tT %n", ( 1000L * Integer.MAX_VALUE ) );
-        System.out.printf( "%tF %<tT %n", ( 1000L * Integer.MIN_VALUE ) );
+        int t = 183;
+        int[] arr = { 180, 3, 181, 2, 4 };
+
+        Set<Integer> exists = new HashSet<>();
+        Set<Integer> set = new HashSet<>();
+        IntStream.of( arr )
+                .forEach( set::add );
+        for ( int ind = 0; ind < arr.length; ind++ ) {
+            int i = arr[ind];
+            int tmp = t - i;
+            if ( exists.contains( i ) )
+                continue;
+            if ( set.contains( tmp ) ) {
+                exists.add( i );
+                exists.add( tmp );
+                System.out.println( i + ", " + tmp );
+            }
+        }
     }
 
 }
