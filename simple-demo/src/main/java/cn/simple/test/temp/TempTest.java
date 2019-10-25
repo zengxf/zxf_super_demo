a4721c659f9305883942282284b3a686c2b1a06f
zengxf <fl_zxf@163.com>
2019-10-25 10:45:28 +0800

    测试 lombok 生成构造器

--- simple-demo/src/main/java/cn/simple/test/temp/TempTest.java
+++ simple-demo/src/main/java/cn/simple/test/temp/TempTest.java
@@ -1,10 +1,20 @@
 package cn.simple.test.temp;
 
-// M:\project\zxf_super_demo\simple-demo\bin\main\cn\simple\test\temp
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import lombok.RequiredArgsConstructor;
+
+// M:\zxf-demo-github\zxf_super_demo\simple-demo\bin\main\cn\simple\test\temp
+@RequiredArgsConstructor( onConstructor = @__( { @BB } ) )
 public class TempTest {
 
-    public static void main( String[] args ) {
-       System.out.println( "132".matches( "1\\d{2}" ) );
-    }
+    private final String name;
+    private String       sign;
 
 }
+
+@Retention( RetentionPolicy.RUNTIME )
+@Target( { ElementType.CONSTRUCTOR } ) @interface BB { }
