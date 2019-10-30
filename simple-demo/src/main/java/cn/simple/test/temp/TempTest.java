d98e9fdf9537b695fbcda4ef17327d41c3c33bd3
zengxf <fl_zxf@163.com>
2019-10-30 16:45:19 +0800

    计算 bloom filter 大小

--- simple-demo/src/main/java/cn/simple/test/temp/TempTest.java
+++ simple-demo/src/main/java/cn/simple/test/temp/TempTest.java
@@ -1,62 +1,13 @@
 package cn.simple.test.temp;
 
-import java.util.HashMap;
-import java.util.Map;
-
 // M:\zxf-demo-github\zxf_super_demo\simple-demo\bin\main\cn\simple\test\temp
 public class TempTest {
 
     public static void main( String[] args ) {
-
-//        System.out.println(test4());
-        System.out.println(getMap().get("KEY").toString());
+        double p = 0.001;
+        int n = 200;
+        long v = (long) ( -n * Math.log( p ) / ( Math.log( 2 ) * Math.log( 2 ) ) );
+        System.out.println( v );
     }
-
-    public static Map<String, String> getMap() {
-        Map<String, String> map = new HashMap<String, String>();
-        map.put("KEY", "INIT");
-
-        try {
-            map.put("KEY", "TRY");
-            return map;
-        }
-        catch (Exception e) {
-            map.put("KEY", "CATCH");
-        }
-        finally {
-            map.put("KEY", "FINALLY");
-            map = null;
-        }
-
-        return map;
-    }
-
-    public static int test4() {
-        int b = 20;
-
-        try {
-            System.out.println("try block");
-
-            b = b / 0;
-
-            return b += 80;
-        } catch (Exception e) {
-
-            b += 15;
-            System.out.println("catch block");
-        } finally {
-
-            System.out.println("finally block");
-
-            if (b > 25) {
-                System.out.println("b>25, b = " + b);
-            }
-
-            b += 50;
-        }
-
-        return 204;
-    }
-
 
 }
