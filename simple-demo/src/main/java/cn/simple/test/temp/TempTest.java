d423e28d7c89b31cf27b67bf2da9624155c95e4e
zengxf <fl_zxf@163.com>
2019-10-30 10:17:49 +0800

    finally 测试

--- simple-demo/src/main/java/cn/simple/test/temp/TempTest.java
+++ simple-demo/src/main/java/cn/simple/test/temp/TempTest.java
@@ -1,20 +1,38 @@
 package cn.simple.test.temp;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
-import lombok.RequiredArgsConstructor;
-
 // M:\zxf-demo-github\zxf_super_demo\simple-demo\bin\main\cn\simple\test\temp
-@RequiredArgsConstructor( onConstructor = @__( { @BB } ) )
 public class TempTest {
 
-    private final String name;
-    private String       sign;
+    public static void main( String[] args ) {
+
+        System.out.println( test5() );
+    }
+
+    public static int test5() {
+        int b = 20;
+
+        try {
+            System.out.println( "try block" );
+
+            b = b / 0;
+
+            return b += 80;
+        } catch ( Exception e ) {
+
+            System.out.println( "catch block" );
+            return b += 15;
+        } finally {
+
+            System.out.println( "finally block" );
+
+            if ( b > 25 ) {
+                System.out.println( "b>25, b = " + b );
+            }
+            // 说明了发生异常后，catch中的return语句先执行，确定了返回值后再去执行finally块，执行完了catch再返回，finally里对b的改变对返回值无影响
+            b += 50;
+        }
+
+        // return b;
+    }
 
 }
-
-@Retention( RetentionPolicy.RUNTIME )
-@Target( { ElementType.CONSTRUCTOR } ) @interface BB { }
