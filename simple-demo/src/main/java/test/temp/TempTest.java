7a63abb3956686fbd08f69135f6b8f5a48babfb0
zengxf <fl_zxf@163.com>
2019-12-04 12:01:17 +0800

    JOL

--- simple-demo/src/main/java/test/temp/TempTest.java
+++ simple-demo/src/main/java/test/temp/TempTest.java
@@ -1,15 +1,7 @@
 package test.temp;
 
-import java.util.List;
-
 // M:\zxf-demo-github\zxf_super_demo\simple-demo\bin\main\test\temp
-// java -jar jol-cli.jar internals -cp . test.temp.TempTest
-// java -jar jol-cli.jar internals -cp M:\zxf-demo-github\zxf_super_demo\simple-demo\bin\main test.temp.TempTest
 public class TempTest {
-
-    private String       aa;
-    private List<String> list;
-    private int          aab;
 
     public static void main( String[] args ) {
         System.out.println( "sssssssss" );
