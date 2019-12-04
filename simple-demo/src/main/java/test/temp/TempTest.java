2206655e27ebd9d9d09cf7e946892ac2cf85d06b
zengxf <fl_zxf@163.com>
2019-12-04 18:02:32 +0800

    小算法

--- simple-demo/src/main/java/test/temp/TempTest.java
+++ simple-demo/src/main/java/test/temp/TempTest.java
@@ -4,7 +4,29 @@ package test.temp;
 public class TempTest {
 
     public static void main( String[] args ) {
-        System.out.println( "sssssssss" );
+        System.out.println( multiplication( 100 ) );
+        System.out.println( formulaTotal( 4 ) );
     }
 
+    static double formulaTotal( long n ) {
+        double sum = 0;
+        for ( long i = 1; i < n; i++ ) {
+            sum += formula( n, i );
+        }
+        sum += 1;
+        return sum;
+    }
+
+    static double formula( long n, long m ) {
+        return multiplication( n ) / ( multiplication( m ) * multiplication( n - m ) );
+    }
+
+    static double multiplication( long v ) {
+        double m = 1;
+        while ( v > 0 ) {
+            m *= v;
+            v--;
+        }
+        return m;
+    }
 }
