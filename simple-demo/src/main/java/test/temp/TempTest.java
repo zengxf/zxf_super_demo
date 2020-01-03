54869e9b146002fffd9914ebab884f3081a0c697
zengxf <fl_zxf@163.com>
2020-01-03 15:08:02 +0800

    temp

--- simple-demo/src/main/java/test/temp/TempTest.java
+++ simple-demo/src/main/java/test/temp/TempTest.java
@@ -1,60 +1,12 @@
 package test.temp;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.time.LocalDate;
 
 // M:\zxf-demo-github\zxf_super_demo\simple-demo\bin\main\test\temp
 public class TempTest {
 
-    public static void main( final String[] args ) {
-        List<int[]> list = new ArrayList<>();
-        printTotal( 4, list );
-        List<String> strList = list.stream()
-                .map( Arrays::toString )
-                .collect( Collectors.toList() );
-        System.out.println( strList );
-        System.out.println( strList.size() );
-    }
-
-    public static final void printTotal( final int n, List<int[]> list ) {
-        for ( int i = 1; i <= n; i++ ) {
-            print_nCr( n, i, list );
-        }
-    }
-
-    public static final void print_nCr( final int n, final int r, List<int[]> list ) {
-        int[] res = new int[r];
-        for ( int i = 0; i < res.length; i++ ) {
-            res[i] = i + 1;
-        }
-        boolean done = false;
-        while ( !done ) {
-            int[] newArr = Arrays.copyOf( res, res.length );
-            list.add( newArr );
-            done = getNext( res, n, r );
-        }
-    }
-
-    public static final boolean getNext( final int[] num, final int n, final int r ) {
-        int target = r - 1;
-        num[target]++;
-        if ( num[target] > ( ( n - ( r - target ) ) + 1 ) ) {
-            while ( num[target] > ( ( n - ( r - target ) ) ) ) {
-                target--;
-                if ( target < 0 ) {
-                    break;
-                }
-            }
-            if ( target < 0 ) {
-                return true;
-            }
-            num[target]++;
-            for ( int i = target + 1; i < num.length; i++ ) {
-                num[i] = num[i - 1] + 1;
-            }
-        }
-        return false;
+    public static void main( String[] args ) throws Exception {
+        System.out.println( LocalDate.now()
+                .minusDays( 45 ) );
     }
 }
