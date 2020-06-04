ae0a0e68dafea134c13d8f760bd78993f9ba3053
zengxf <fl_zxf@163.com>
2020-06-04 17:00:42 +0800

    Test class load

--- simple-demo/src/main/java/test/temp/TempTest.java
+++ simple-demo/src/main/java/test/temp/TempTest.java
@@ -1,10 +1,29 @@
 package test.temp;
 
+import lombok.extern.slf4j.Slf4j;
+
 // M:\zxf-demo-github\zxf_super_demo\simple-demo\bin\main\test\temp
+@Slf4j
 public class TempTest {
 
-    public static void main( String[] args ) throws Exception {
-        System.out.println( "sssssss" );
+    private int a = 2;
+    private int b;
+    private int c;
+
+    {
+        b = 3;
+        log.info( "普通块" );
+        log.info( "{},{},{}", a, b, c );
     }
-    
+
+    TempTest() {
+        c = 44;
+        log.info( "构造函数" );
+        log.info( "{},{},{}", a, b, c );
+    }
+
+    public static void main( String[] args ) throws Exception {
+        new TempTest();
+    }
+
 }
